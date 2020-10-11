aa358d6c86b60f84a6b8e413e2634a692cd30b45
dineshk2406 <dinesh.bobba@gmail.com>
2020-10-11 18:38:15 +0530

    Sum of two num program

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Arithmetic
+{
+	public static void main(String[] args)
+	{ 
+		int a=5;
+		int b=4;
+		int c;
+		c= a+b;
+	System.out.println("Sum of two numbers a+b: " +c);
+	}
+}
